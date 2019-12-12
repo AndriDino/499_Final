2cb8c2716ba733df883454f53bfa8eb97c506a2c
Dino Andrighetto <dinoghetto@gmail.com>
2019-12-12 13:24:45 -0800

    Updates

---
+++ precache-manifest.19803b433ddd21b716418971aaf64139.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "13610b8f4fbb41b7ace0c799f92deea6",
+    "url": "/AndriDino/499_Final/index.html"
+  },
+  {
+    "revision": "48e826e5dc32db701bce",
+    "url": "/AndriDino/499_Final/static/css/main.6c8ec736.chunk.css"
+  },
+  {
+    "revision": "481a4f4e65f2fac4a40b",
+    "url": "/AndriDino/499_Final/static/js/2.d6feacd6.chunk.js"
+  },
+  {
+    "revision": "48e826e5dc32db701bce",
+    "url": "/AndriDino/499_Final/static/js/main.74d9f055.chunk.js"
+  },
+  {
+    "revision": "7bd36926bb6c22afabb3",
+    "url": "/AndriDino/499_Final/static/js/runtime-main.2e8eba14.js"
+  }
+]);
